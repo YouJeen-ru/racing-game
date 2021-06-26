deb213988103d3afc9ec014fe44579ea02bd2fb3
YouJeen-ru <1.rouch@mail.ru>
2021-06-26 18:52:02 +0300

    impact cars realized

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ const getRandomEnemy = (max) =>  Math.floor((Math.random() * max) + 1)
 function startGame() {
     gameArea.style.minHeight = 100 + 'vh'
     start.classList.add('hide')
+    gameArea.innerHTML = ''
 
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div')
@@ -52,8 +53,12 @@ function startGame() {
         enemy.style.background = `transparent url(./image/enemy${getRandomEnemy(MAX_ENEMY)}.png) center / cover no-repeat`
         gameArea.appendChild(enemy)
     }
+    setting.score = 0
     setting.start = true
     gameArea.appendChild(car)
+    car.style.left = '125px';
+    car.style.top = 'auto'
+    car.style.bottom = '10px'
     setting.x = car.offsetLeft
     setting.y = car.offsetTop
     requestAnimationFrame(playGame)
@@ -61,6 +66,8 @@ function startGame() {
 
 function playGame() {
     if (setting.start) {
+        setting.score += setting.speed
+        score.innerHTML = "SCORE<br/>" + setting.score
         moveRoad()
         moveEnemy()
         if (keys.ArrowLeft && setting.x > 0) {
@@ -115,6 +122,15 @@ function moveRoad() {
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy')
     enemy.forEach(function (enemyItem) {
+        let carRect = car.getBoundingClientRect()
+        let enemyRect = enemyItem.getBoundingClientRect()
+
+        if (carRect.top <= enemyRect.bottom && carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right && carRect.bottom >= enemyRect.top) {
+            setting.start = false
+            start.classList.remove('hide')
+            start.style.top = '70px'
+        }
         enemyItem.y += setting.speed / 2
         enemyItem.style.top = enemyItem.y + 'px'
         if (enemyItem.y >= document.documentElement.clientHeight) {
